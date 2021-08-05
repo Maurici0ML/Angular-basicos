aeb3e366529303ad9d2a280fba45923a4cf35cf0
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2021-08-05 16:57:27 -0500

    FEAT: primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'] | Esto toma el css que es solo para el appComponent
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
